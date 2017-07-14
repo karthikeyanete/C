8a1898b9dd2a0052835f8da2dfbcbf274af878eb
karthikeyan <karthikeyan.r30497@gmail.com>
2017-07-14 23:43:11 +0530

    Update and rename Union.c to Repeated Element in each row.c

--- Repeated Element in each row.c
+++ Repeated Element in each row.c
@@ -1,3 +1,17 @@
+/* To find the repeated rlrment in the each row 
+Sample Input:
+5 5
+1 2 3 4 5
+6 4 4 4 5
+1 1 1 5 4
+1 2 2 2 5
+1 2 3 4 5
+Sample output:
+5
+*/
+
+
+
 #include <stdio.h>
 int main() {
 	int row,column,i,j,hash[100]={0},count=0,value=0;
